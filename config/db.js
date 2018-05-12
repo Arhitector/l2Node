266071cc6ba1a2dda351e541ab344174135c6c3f
Unknown <tantalidovo@mail.ru>
2018-05-12 18:08:36 +0200

    server mongo

---
+++ config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+  url : 'mongodb://root:creator666@ds119060.mlab.com:19060/l2db',
+};
--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
+const db             = require('./config/db');
 const app            = express();
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
 
 const port = 8000;
-app.listen(port, () => {
-  console.log('We are live on ' + port);
-});
+app.use(bodyParser.urlencoded({ extended: true }));
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err)
+  require('./app/routes')(app, database);
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
+  });               
+})
